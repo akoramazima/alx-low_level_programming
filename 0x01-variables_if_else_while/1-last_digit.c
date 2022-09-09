5b32c1d774f71504c11320ef4697ae835d11c135
akoramazima <akoramazimabruce@gmail.com>
2022-09-09 14:23:39 -0700

    quest 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <ctype.h>
+
 /**
-* main - print if the number is positive, zeero, or negative
-*
-* Description: using the main function
-* this program prints "programming is positive, zero, and negative
+* main - program that prints the alphabet in lowercase, followed by a new line
+* You can only use the putchar
 * Return: 0
 */
+
 int main(void)
 {
-int n;
-int l;
-
-srand(time(0));
-n = rand( ) - RAND_MAX / 2;
-l = n % 10;
-
-if (1 > 5)
+int l = 'a';
+while (l <= 'z')
 {
-        printf("last digit of %d is %d and is greater than s\n", n, l);
+putchar(l);
+l += 1;
 }
-else if (1 == 0)
-{
-       printf("last digit of %d is %d and is 0\n", n, l);
-}
-else
-{
-      printf("last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
-return(0);
+putchar('\n');
+return (0);
 }
