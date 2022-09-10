6724412ccd918a81fce6941349b0e93da8c341b1
akoramazima <akoramazimabruce@gmail.com>
2022-09-10 13:56:30 -0700

    programming 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-#include <ctype.h>
 
 /**
-* main - program that prints the alphabet in lowercase, followed by a new line
-* You can only use the putchar
+* main - assign a random number to the variable n each time it is executed
+* The string Last digit of n
 * Return: 0
 */
 
 int main(void)
 {
-int l = 'a';
-while (l <= 'z')
+int n;
+int lastn;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastn = n % 10;
+if (lastn > 5)
 {
-putchar(l);
-l += 1;
+printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 }
-putchar('\n');
+else if (lastn == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastn);
+}
+else if (lastn < 6 && lastn != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
+}
 return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /**
-* main - program that prints the alphabet in lowercase, and then in uppercase,
+* main - program that prints the alphabet in lowercase, followed by a new line
 * You can only use the putchar
 * Return: 0
 */
 
 int main(void)
 {
-int lw = 'a';
-int up = 'A';
-while (lw <= 'z')
+int l = 'a';
+while (l <= 'z')
 {
-putchar(lw);
-lw += 1;
-}
-while (up <= 'Z')
-{
-putchar(up);
-up += 1;
+putchar(l);
+l += 1;
 }
 putchar('\n');
 return (0);
