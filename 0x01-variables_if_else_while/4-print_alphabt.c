a2a6bae3d286b168235c52faf1006abe2dcd8889
akoramazima <akoramazimabruce@gmail.com>
2022-09-10 12:26:04 -0700

    proramming c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - program that prints the alphabet in lowercase, and then in uppercase,
+* main - program that prints the alphabet in lowercase, followed by a new line
 * You can only use the putchar
 * Return: 0
 */
@@ -9,16 +9,17 @@
 int main(void)
 {
 int lw = 'a';
-int up = 'A';
 while (lw <= 'z')
+{
+if (lw == 'e' || lw == 'q')
+{
+lw += 1;
+}
+else
 {
 putchar(lw);
 lw += 1;
 }
-while (up <= 'Z')
-{
-putchar(up);
-up += 1;
 }
 putchar('\n');
 return (0);
