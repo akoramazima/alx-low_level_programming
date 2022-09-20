93579f689ead5648afaaa0304b8223e5c7d5def1
akoramazima <akoramazimabruce@gmail.com>
2022-09-19 17:40:05 -0700

    q8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,14 @@
 void print_array(int *a, int n)
 {
 int inc;
+
 for (inc = 0; inc < n ; inc++)
 {
-if (inc  != n -1)
+if (inc  != n - 1)
 printf("%d, ", a[inc]);
 else
 printf("%d", a[inc]);
 }
 putchar(0);
 }
+
