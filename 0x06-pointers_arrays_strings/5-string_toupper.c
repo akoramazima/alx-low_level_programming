5f6a41b6c7a4ed939a8178816b3e024a2bc4e5fa
akoramazima <akoramazimabruce@gmail.com>
2022-09-22 00:17:50 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,7 +4,7 @@
  * string_toupper - changes all lowercase letters to uppercase
  * @str: string to be changed
  *
- * Return: address tp the string
+ * Return: address to the string
  */
 
 char *string_toupper(char *str)
@@ -13,7 +13,7 @@ char *string_toupper(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] >= '0' && str[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
 		i++;
 	}
