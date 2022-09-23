79f8845e80261c3d4f97dc65b10d44e248471658
akoramazima <akoramazimabruce@gmail.com>
2022-09-23 14:48:33 -0700

    f

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,15 @@
 
 void print_most_numbers(void)
 {
-		int n;
+	int n;
 
-			for (n = 48; n < 58; n++)
-					{
-								if ((n == 50) || (n == 52))
-											{
-															continue;
-																	}
-										putchar(n);
-											}
-				putchar(10);
+	for (n = 48; n < 58; n++)
+	{
+	if ((n == 50) || (n == 52))
+	{
+	continue;
+	}
+	putchar(n);
+	}
+	putchar(10);
 }
