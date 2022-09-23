68ec0c1eafbd994b1db7e560cd8794a082f75d11
akoramazima <akoramazimabruce@gmail.com>
2022-09-23 15:32:07 -0700

    g

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,29 +8,26 @@
 
 void print_triangle(int size)
 {
-		int inc1, inc2;
+	int inc1, inc2;
 
-			if (size > 0)
-					{
-								for (inc1 = 1; inc1 <= size; inc1++)
-											{
-															for ((inc2 = size - inc1); inc2 > 0; inc2--)
-																			{
-																								putchar(' ');
-																											}
-
-																		for (inc2 = 0; inc2 < inc1; inc2++)
-																						{
-																											putchar('#');
-																														}
-
-																					if (inc1 == size)
-																									{
-																														continue;
-																																	}
-
-																								putchar('\n');
-																										}
-									}
-				putchar('\n');
+	if (size > 0)
+	{
+		for (inc1 = 1; inc1 <= size; inc1++)
+		{
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+			{
+				putchar(' ');
+			}
+			for (inc2 = 0; inc2 < inc1; inc2++)
+			{
+				putchar('#');
+			}
+			if (inc1 == size)
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
 }
