5e74faf08445e40fee2a627d9f9b8500a6df6256
akoramazima <akoramazimabruce@gmail.com>
2022-09-28 07:49:52 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _put_recursion - prints a string followed by a new line
+ * @s string to be printed
+ *
+ * Return: void
+ */
+Prototype: void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
