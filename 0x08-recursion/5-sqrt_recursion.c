48310f6a883e95fb4b85e74a58c65cc0a70917c8
akoramazima <akoramazimabruce@gmail.com>
2022-09-28 16:45:19 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,3 +12,19 @@ int _sqrt_recursion(int n)
 		return (n);
 	return (_sqrt(0, n));
 }
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: test number
+ * @x: square number
+ *
+ * Return: the square root of n
+ */
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
+		return (-1);
+	else if (n *n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
+}
