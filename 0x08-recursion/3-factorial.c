26876908ddf0eef72a8fd13abf941a542313c8c7
akoramazima <akoramazimabruce@gmail.com>
2022-09-28 18:33:55 -0700

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,10 +10,10 @@ int factorial(int n)
 {
 	 int next_factorial;
 
-	 if (n < 0)
-	 return (-1);
-	 else if (n == 0)
-	 return (1);
-	 next_factorial = factorial(n - 1);
-	 return (n * next_factorial);
+if (n < 0)
+return (-1);
+else if (n == 0)
+return (1);
+next_factorial = factorial(n - 1);
+return (n * next_factorial);
 }
