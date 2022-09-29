88bfe3a20883a4935aeb499f9e69cf99f3d3c511
akoramazima <akoramazimabruce@gmail.com>
2022-09-28 17:00:55 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,7 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt - returns the square root of a number
  * @n: test number
  * @x: square number
  *
