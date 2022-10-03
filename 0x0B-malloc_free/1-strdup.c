95b204cf22cb06d3d62f524fdb5e4facbadf930d
akoramazima <akoramazimabruce@gmail.com>
2022-10-03 04:30:06 -0700

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+ * _strdup - returns a pointer to a newly allocated space
+ *  in memory, which contains a copy of the string
+ *  given as a parameter.
  * @str: string to be copied
  *
  * Return:  NULL in case of error, pointer to allocate space
