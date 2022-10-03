57d362698808cb1eafae0faaae714c618b5090dd
akoramazima <akoramazimabruce@gmail.com>
2022-10-03 09:41:13 -0700

    6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -58,7 +58,7 @@ int num_words(char *str)
 char **strtow(char *str)
 {
 	char **split;
-	int i, j = 0, temp = 0, words = num_words(str);
+	int i, j = 0, temp = 0, size = 0, words = num_words(str);
 
 	if (words == 0)
 		return (NULL);
