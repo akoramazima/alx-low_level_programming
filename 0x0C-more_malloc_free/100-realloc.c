79eb7ec2c399dd9a7ce6a135112e03683c71b74d
akoramazima <akoramazimabruce@gmail.com>
2022-10-06 06:42:00 -0700

    4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,8 +25,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	else if (new_size == old_size);
-	return (ptr);
+	else if (new_size == old_size)
+		return (ptr);
 
 	p = malloc(new_size);
 	if (p == NULL)
