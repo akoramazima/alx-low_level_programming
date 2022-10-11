657aadcbd01423e0a055be1e9e8a19a64e67255d
akoramazima <akoramazimabruce@gmail.com>
2022-10-10 17:44:30 -0700

    r

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @owner: the owner of the dog.
  */
 
-void it_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -24,5 +24,5 @@ void print_dog(struct dog *d)
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
-		print("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 }
