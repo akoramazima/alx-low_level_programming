91ce6bbb5e0af92d3268a78cdb130a2c466763ab
akoramazima <akoramazimabruce@gmail.com>
2022-10-10 17:57:52 -0700

    r

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,7 +33,7 @@ char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
 
-	for (index =0; src[index]; index++)
+	for (index = 0; src[index]; index++)
 		dest[index] = src[index];
 
 	dest[index] = '\0';
