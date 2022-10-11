1b913815fc3846e442020a848258560d0d090d60
akoramazima <akoramazimabruce@gmail.com>
2022-10-10 17:52:42 -0700

    r

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -69,14 +69,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (doggo->owner == NULL)
 	{
-		free(doggo->name, name);
+		free(doggo->name);
 		free(doggo);
 		return (NULL);
 	}
 
 	doggo->name = _strcopy(doggo->name, name);
 	doggo->age = age;
-	doggo-owner = _strcopy(doggo->owner, owner);
+	doggo->owner = _strcopy(doggo->owner, owner);
 
 	return (doggo);
 }
