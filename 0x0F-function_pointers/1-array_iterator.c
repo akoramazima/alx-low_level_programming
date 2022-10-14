6d078336642be5b5dad61fb12985cdee1027db45
akoramazima <akoramazimabruce@gmail.com>
2022-10-14 13:48:51 -0700

    g

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -40,8 +40,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-
-	if (array && action)		
+	
+	if (array && action)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
